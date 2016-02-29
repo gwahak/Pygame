2b3f42f938d68e2e00f9877f4e5b852abb2e40b1
Chan, Danny <danny50610@yahoo.com.tw>
2016-02-29 15:00:46 +0800

    建立井字遊戲的基本架構

---
+++ Lession02/Tic-tac-toe/Source.py
@@ -0,0 +1,43 @@
+import pygame
+
+going = True
+screen = None
+clock = None
+font = None
+
+
+def main():
+    pygame.init()
+
+    while going:
+        update()
+        draw()
+        clock.tick(60)
+
+    pygame.quit()
+
+
+def init():
+    global screen, clock, font
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("井字遊戲")
+    clock = pygame.time.Clock()
+    font = pygame.font.Font(r"C:\Windows\Fonts\consola.ttf", 36)
+
+
+def update():
+    global going
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            going = False
+
+
+def draw():
+    global screen, clock, font
+
+    screen.fill((0, 0, 0))
+    screen.blit(font.render(str(clock.get_fps()), True, (255, 255, 255)), (10, 10))
+    pygame.display.update()
+
+if __name__ == '__main__':
+    main()
