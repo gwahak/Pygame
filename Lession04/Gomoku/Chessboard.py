4505c0999b4095152c7e8b251f7a8f711259a309
Chan, Danny <danny50610@yahoo.com.tw>
2016-03-13 22:51:28 +0800

    去除魔術數字 提出常數到class層級

--- Lession04/Gomoku/Chessboard.py
+++ Lession04/Gomoku/Chessboard.py
@@ -5,19 +5,20 @@ class Chessboard:
 
     def __init__(self):
         self.grid_size = 25
-
+        self.start_x, self.start_y = 30, 50
+        self.edge_size = 15
+        self.grid_count = 19
 
     def draw(self, screen):
-        startX, startY = 30, 50
-
+        # 棋盤底色
         pygame.draw.rect(screen, (185, 122, 87),
-                         [startX - 15, startY - 15,
-                          (19 - 1) * self.grid_size + 30, (19 - 1) * self.grid_size + 30], 0)
+                         [self.start_x - self.edge_size, self.start_y - self.edge_size,
+                          (self.grid_count - 1) * self.grid_size + self.edge_size * 2, (self.grid_count - 1) * self.grid_size + self.edge_size * 2], 0)
 
-        for r in range(19):
-            y = startY + r * self.grid_size
-            pygame.draw.line(screen, (0, 0, 0), [startX, y], [startX + self.grid_size * (19 - 1), y], 2)
+        for r in range(self.grid_count):
+            y = self.start_y + r * self.grid_size
+            pygame.draw.line(screen, (0, 0, 0), [self.start_x, y], [self.start_x + self.grid_size * (self.grid_count - 1), y], 2)
 
-        for c in range(19):
-            x = startX + c * self.grid_size
-            pygame.draw.line(screen, (0, 0, 0), [x, startY], [x, startY + self.grid_size * (19 - 1)], 2)
+        for c in range(self.grid_count):
+            x = self.start_x + c * self.grid_size
+            pygame.draw.line(screen, (0, 0, 0), [x, self.start_y], [x, self.start_y + self.grid_size * (self.grid_count - 1)], 2)
