704192099385f8d451bf9e6ad48d3141907ca08b
Chan, Danny <danny50610@yahoo.com.tw>
2016-03-13 22:48:39 +0800

    將 grid_size 設為 class 成員

--- Lession04/Gomoku/Chessboard.py
+++ Lession04/Gomoku/Chessboard.py
@@ -4,23 +4,20 @@ import pygame
 class Chessboard:
 
     def __init__(self):
-        pass
+        self.grid_size = 25
 
-    def update(self):
-        pass
 
     def draw(self, screen):
         startX, startY = 30, 50
-        grid_size = 25
 
         pygame.draw.rect(screen, (185, 122, 87),
                          [startX - 15, startY - 15,
-                          (19 - 1) * grid_size + 30, (19 - 1) * grid_size + 30], 0)
+                          (19 - 1) * self.grid_size + 30, (19 - 1) * self.grid_size + 30], 0)
 
         for r in range(19):
-            y = startY + r * grid_size
-            pygame.draw.line(screen, (0, 0, 0), [startX, y], [startX + grid_size * (19 - 1), y], 2)
+            y = startY + r * self.grid_size
+            pygame.draw.line(screen, (0, 0, 0), [startX, y], [startX + self.grid_size * (19 - 1), y], 2)
 
         for c in range(19):
-            x = startX + c * grid_size
-            pygame.draw.line(screen, (0, 0, 0), [x, startY], [x, startY + grid_size * (19 - 1)], 2)
+            x = startX + c * self.grid_size
+            pygame.draw.line(screen, (0, 0, 0), [x, startY], [x, startY + self.grid_size * (19 - 1)], 2)
