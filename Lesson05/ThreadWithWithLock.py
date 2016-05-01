018ae60c2649d691db860ae3531a109b2eac71ed
Chan, Danny <danny50610@yahoo.com.tw>
2016-05-01 22:34:47 +0800

    Add ThreadWithWithLock

---
+++ Lesson05/ThreadWithWithLock.py
@@ -0,0 +1,27 @@
+from threading import Thread, Lock
+
+pos = 1000
+
+
+class UpdatePos(Thread):
+    def __init__(self, lock, offset):
+        super().__init__()
+        self.lock = lock
+        self.offset = offset
+
+    def run(self):
+        global pos
+        for i in range(1000000):
+            with self.lock:
+                pos += self.offset
+
+
+if __name__ == '__main__':
+    lock = Lock()
+    t1 = UpdatePos(lock, 100)
+    t2 = UpdatePos(lock, -100)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+    print(pos)
