6023dde39114db969c85d83a90843131ce93059e
Chan, Danny <danny50610@yahoo.com.tw>
2016-05-04 11:02:44 +0800

    Add CalcPi

---
+++ Lesson05/CalcPi.py
@@ -0,0 +1,32 @@
+import random
+import math
+
+import time
+
+
+def calc_pi(point_count):
+    in_circle_count = 0
+    for i in range(point_count):
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        dist = x ** 2 + y ** 2
+        if dist <= 1 ** 2:
+            in_circle_count += 1
+    return in_circle_count * 4 / point_count
+
+
+def get_error_rate(found, theory):
+    return abs(found - theory) / theory
+
+
+def main():
+    start = time.process_time()
+    ans = calc_pi(1000000)
+    end = time.process_time()
+
+    print("PI = {0:.6f}({1:.2f}%)".format(ans, get_error_rate(ans, math.pi) * 100))
+    print(end - start)
+
+
+if __name__ == '__main__':
+    main()
